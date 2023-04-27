db81f9fc84be737badef6f22a864e0275fcf6cb9
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2023-04-27 14:11:27 +0200

    layout changes

--- client/src/components/layout/header/Header.tsx
+++ client/src/components/layout/header/Header.tsx
@@ -1,20 +1,46 @@
 import Logo from '../Logo.jsx';
 import { FC } from 'react';
+import { useLocation } from 'react-router';
 
 import { Link } from 'react-router-dom';
-import './Header.module.scss';
+import styles from './Header.module.scss';
 
-const Header: FC = (): JSX.Element => <header>
-    <div className="container flex justify-between items-center">
+const Header: FC = (): JSX.Element => {
+  const location = useLocation();
+
+  console.log(location.pathname);
+
+  return (
+    <header>
+      <div className="container flex justify-between items-center">
         <Link to="./">
-            <Logo fill="#FF6060" /></Link>
+          <Logo fill="#FF6060" style={styles['logo']} />
+        </Link>
         <nav>
-            <ul className="flex justify-between">
-                <li><Link to="./">Accueil</Link></li>
-                <li><Link to="./a-propos">A propos</Link></li>
-            </ul>
+          <ul className="flex justify-between">
+            <li>
+              <Link
+                to="./"
+                className={location.pathname === '/' ? 'active' : 'inactive'}
+              >
+                Accueil
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="./a-propos"
+                className={
+                  location.pathname === '/a-propos' ? 'active' : 'inactive'
+                }
+              >
+                A propos
+              </Link>
+            </li>
+          </ul>
         </nav>
-    </div>
-</header>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
