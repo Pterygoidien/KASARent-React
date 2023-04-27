ad63b4e48301280d696e5f92c6ee4ea947f86d69
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2023-04-27 13:11:32 +0200

    changing about page

--- client/src/components/common/banner/Banner.tsx
+++ client/src/components/common/banner/Banner.tsx
@@ -1,15 +1,15 @@
-import { FC } from "react";
-import styles from "./Banner.module.scss";
+import { FC } from 'react';
+import styles from './Banner.module.scss';
 
 const Banner: FC = (): JSX.Element => {
-    return (
-        <section className={styles.banner}>
-            <h1 className="flex wrap justify-start">
-                <span className="part1">Chez vous, </span>
-                <span className="part2">partout et ailleurs</span>
-            </h1>
-        </section>
-    );
-}
+  return (
+    <section className={styles.banner}>
+      <h1 className="flex wrap justify-start text-white">
+        <span className="part1">Chez vous, </span>
+        <span className="part2">partout et ailleurs</span>
+      </h1>
+    </section>
+  );
+};
 
 export default Banner;
---
+++ client/src/views/pages/about/Banner.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import styles from './Banner.module.scss';
+
+const Banner: FC = (): JSX.Element => {
+  return <section className={styles.banner}></section>;
+};
+
+export default Banner;
