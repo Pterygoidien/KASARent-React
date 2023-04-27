51782fca5ee2ebd39d11c513e5018b4c6a1d7bfe
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2023-04-27 14:33:59 +0200

    last commit changes part 1é

--- client/src/components/common/gallery/Gallery.jsx
+++ client/src/components/common/gallery/Gallery.jsx
@@ -1,31 +1,30 @@
-import GalleryArticle from "./GalleryArticle";
-import { useEffect } from "react";
-import { useContext } from "react";
-import LogementContext from "../../../store/logement-context/logementContext";
-import Spinner from "../../layout/Spinner";
+import GalleryArticle from './GalleryArticle';
+import { useEffect } from 'react';
+import { useContext } from 'react';
+import LogementContext from '../../../store/logement-context/logementContext';
+import Spinner from '../../layout/Spinner';
 
-import styles from "./Gallery.module.scss";
-
+import styles from './Gallery.module.scss';
 
 const Gallery = () => {
+  const { logements, getLogements, loading } = useContext(LogementContext);
 
-    const { logements, getLogements, loading } = useContext(LogementContext);
+  //On react Strict Mode, useEffect depending on an empty array is called twice on first render : two API calls
+  useEffect(() => {
+    getLogements();
+    // eslint-disable-next-line
+  }, []);
 
-    //On react Strict Mode, useEffect depending on an empty array is called twice on first render : two API calls
-    useEffect(() => {
-        getLogements();
-        // eslint-disable-next-line
-    }, []);
-
-
-    if (loading) return <Spinner />
-    return (
-        <section className={`${styles.gallery} rounded-2xl items flex gap-6 flex-expand flex-center`}>
-            {logements && logements.map((logement) => (
-                <GalleryArticle key={logement.id} data={logement} />
-            ))}
-        </section>
-
-    )
-}
+  if (loading) return <Spinner />;
+  return (
+    <section
+      className={`${styles.gallery} rounded-2xl items flex gap-10 flex-expand flex-center`}
+    >
+      {logements &&
+        logements.map((logement) => (
+          <GalleryArticle key={logement.id} data={logement} />
+        ))}
+    </section>
+  );
+};
 export default Gallery;
